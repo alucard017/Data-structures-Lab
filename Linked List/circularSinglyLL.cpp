1f1922e2c8a7708e31c2b6ce6947320f4279eb89
Apurba Sundar Nayak <76940364+alucard017@users.noreply.github.com>
2024-04-25 00:58:39 +0530

    new Files added

--- Linked List/circularSinglyLL.cpp
+++ Linked List/circularSinglyLL.cpp
@@ -7,15 +7,20 @@ struct Node
     Node *next;
 };
 
+char ch;
+int data, position, el;
+
 Node *insertBeginning(Node *head, int data)
 {
     Node *newNode = new Node;
     newNode->data = data;
+    newNode->next = newNode;
+
     if (head == nullptr)
     {
-        newNode->next = newNode;
         return newNode;
     }
+
     Node *temp = head;
     while (temp->next != head)
     {
@@ -30,11 +35,13 @@ Node *insertEnd(Node *head, int data)
 {
     Node *newNode = new Node;
     newNode->data = data;
+    newNode->next = newNode;
+
     if (head == nullptr)
     {
-        newNode->next = newNode;
         return newNode;
     }
+
     Node *temp = head;
     while (temp->next != head)
     {
@@ -45,37 +52,143 @@ Node *insertEnd(Node *head, int data)
     return head;
 }
 
-Node *deleteBeginning(Node *head)
+Node *insertSpecific(Node *head, int data, int position)
+{
+    if (position <= 0)
+    {
+        cout << "Invalid position. Position should be positive." << endl;
+        return head;
+    }
+
+    if (position == 1)
+    {
+        return insertBeginning(head, data);
+    }
+
+    Node *newNode = new Node;
+    newNode->data = data;
+
+    Node *temp = head;
+    for (int i = 1; i < position - 1 && temp != nullptr; i++)
+    {
+        temp = temp->next;
+    }
+
+    if (temp == nullptr)
+    {
+        cout << "Position out of range." << endl;
+        return head;
+    }
+
+    newNode->next = temp->next;
+    temp->next = newNode;
+    return head;
+}
+
+int deleteBeginning(Node *&head)
 {
     if (head == nullptr)
     {
         cout << "List is empty" << endl;
-        return nullptr;
+        return -1;
     }
+
+    int data = head->data;
+
     if (head->next == head)
     {
         delete head;
-        return nullptr;
+        return -1;
     }
+
     Node *temp = head;
     while (temp->next != head)
     {
         temp = temp->next;
     }
     temp->next = head->next;
-    Node *temp2 = head;
+    Node *delNode = head;
     head = head->next;
-    delete temp2;
-    return head;
+    delete delNode;
+    return data;
 }
 
-void displayList(Node *head)
+int deleteEnd(Node *&head)
+{
+    if (head == nullptr)
+    {
+        cout << "List is empty" << endl;
+        return -1;
+    }
+
+    int data;
+    if (head->next == head)
+    {
+        data = head->data;
+        delete head;
+        head = nullptr;
+        return data;
+    }
+
+    Node *temp = head;
+    while (temp->next->next != head)
+    {
+        temp = temp->next;
+    }
+    Node *delNode = temp->next;
+    data = delNode->data;
+    temp->next = head;
+    delete delNode;
+    return data;
+}
+
+int deleteSpecific(Node *&head, int position)
+{
+    if (head == nullptr)
+    {
+        cout << "List is empty" << endl;
+        return -1;
+    }
+
+    if (position <= 0)
+    {
+        cout << "Invalid position. Position should be positive." << endl;
+        return -1;
+    }
+
+    if (position == 1)
+    {
+        return deleteBeginning(head);
+    }
+
+    Node *temp = head;
+    for (int i = 1; i < position - 1 && temp != nullptr; i++)
+    {
+        temp = temp->next;
+    }
+
+    if (temp == nullptr || temp->next == head)
+    {
+        cout << "Position out of range." << endl;
+        return -1;
+    }
+
+    Node *delNode = temp->next;
+    int data = delNode->data;
+    temp->next = delNode->next;
+    delete delNode;
+    return data;
+}
+
+void traverseList(Node *head)
 {
     if (head == nullptr)
     {
         cout << "List is empty" << endl;
         return;
     }
+
+    cout << "List is:" << endl;
     Node *temp = head;
     do
     {
@@ -85,58 +198,97 @@ void displayList(Node *head)
     cout << endl;
 }
 
+void deleteList(Node *&head)
+{
+    if (head == nullptr)
+    {
+        cout << "List is already empty" << endl;
+        return;
+    }
+
+    Node *temp = head;
+    Node *nextNode;
+    do
+    {
+        nextNode = temp->next;
+        delete temp;
+        temp = nextNode;
+    } while (temp != head);
+
+    head = nullptr;
+    cout << "List is Deleted" << endl;
+}
+
 int main()
 {
     Node *head = nullptr;
-    char choice;
-    int data;
 
     do
     {
-        cout << "1. Insert at beginning\n";
-        cout << "2. Insert at end\n";
-        cout << "3. Delete from beginning\n";
-        cout << "4. Display the list\n";
-        cout << "5. Exit\n";
-        cout << "Enter your choice: ";
+        cout << "1. Insertion at Beginning\n2. Insertion at End\n3. Insertion at specific position\n"
+             << "4. Deletion from beginning\n5. Deletion from end\n6. Deletion from specific position\n"
+             << "7. Traverse List\n8. Delete the List\n";
+        int choice;
+        cout << "Enter Your Choice: ";
         cin >> choice;
 
         switch (choice)
         {
-        case '1':
-            cout << "Enter data to insert: ";
+        case 1:
+            cout << "Enter element to insert: ";
             cin >> data;
             head = insertBeginning(head, data);
+            cout << "Element inserted at Beginning\n";
             break;
-        case '2':
-            cout << "Enter data to insert: ";
+        case 2:
+            cout << "Enter element to insert: ";
             cin >> data;
             head = insertEnd(head, data);
+            cout << "Element inserted at End\n";
             break;
-        case '3':
-            head = deleteBeginning(head);
+        case 3:
+            cout << "Enter element to insert: ";
+            cin >> data;
+            cout << "Enter Position: ";
+            cin >> position;
+            head = insertSpecific(head, data, position);
             break;
-        case '4':
-            displayList(head);
+        case 4:
+            el = deleteBeginning(head);
+            if (el != -1)
+            {
+                cout << "Deleted Element from Beginning is: " << el << endl;
+            }
             break;
-        case '5':
-            cout << "Exiting program...\n";
+        case 5:
+            el = deleteEnd(head);
+            if (el != -1)
+            {
+                cout << "Deleted Element from End is: " << el << endl;
+            }
+            break;
+        case 6:
+            cout << "Enter the position to delete from: ";
+            cin >> position;
+            el = deleteSpecific(head, position);
+            if (el != -1)
+            {
+                cout << "Deleted Element from Given Position is: " << el << endl;
+            }
+            break;
+        case 7:
+            traverseList(head);
+            break;
+        case 8:
+            deleteList(head);
             break;
         default:
-            cout << "Invalid choice. Please enter again.\n";
+            cout << "Invalid choice\n";
         }
-    } while (choice != '5');
-    Node *temp = head;
-    if (temp != nullptr)
-    {
-        Node *prev = nullptr;
-        do
-        {
-            prev = temp;
-            temp = temp->next;
-            delete prev;
-        } while (temp != head);
-    }
+
+        cout << "Do You Want to continue(Y/N): ";
+        cin >> ch;
+    } while (ch == 'Y' || ch == 'y');
 
     return 0;
 }
